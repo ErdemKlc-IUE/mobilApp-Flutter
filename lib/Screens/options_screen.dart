8d80e64a9cbbb563cb4efb5326e59ac30a76b7fd
Özlem Sevinç Ergül <99902721+ozlemergul@users.noreply.github.com>
2023-12-14 01:47:05 +0300

    home screen added and small changes made to the general view of the app.

---
+++ lib/Screens/options_screen.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class OptionsScreen extends StatefulWidget {
+  const OptionsScreen({super.key});
+
+  @override
+  _OptionsScreenState createState() => _OptionsScreenState();
+}
+
+class _OptionsScreenState extends State<OptionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IconButton(
+        icon: Icon(Icons.insert_emoticon),
+        color: Colors.black,
+        onPressed: () {  },
+      ),
+
+    );
+  }
+}
