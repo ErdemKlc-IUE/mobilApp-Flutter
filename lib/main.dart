8ec5ccc46fbee1449c12d92c4dd39c6eb4f0c7fd
Özlem Sevinç Ergül <99902721+ozlemergul@users.noreply.github.com>
2023-12-15 19:02:18 +0300

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'Screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-
-
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: const FirebaseOptions(apiKey: 'AIzaSyCPVzqo3Oc8vPvJAEzYxrO4EruxVRk8Amw', appId: '1:720857719098:android:f4f10afb2c9834b20cf003', messagingSenderId: '720857719098', projectId: 'se380project-c9ac4', storageBucket: "se380project-c9ac4.appspot.com",));
   runApp(const MyApp());
 }
 
